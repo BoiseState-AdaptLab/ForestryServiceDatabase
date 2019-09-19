5aee9bc0c83530821fb94a7d2c013a546277700b
Peter Henggeler <phenggeler.adm@healthwise.org>
2019-09-19 13:42:27 -0600

    committing first draft of docker setup

---
+++ db/sql-scripts/CreateForest.sql
@@ -0,0 +1,4 @@
+CREATE TABLE forests (
+forest_name varchar(25),
+forest_description  varchar(25)
+);
---
+++ db/sql-scripts/InsertData.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO forests (forest_name, forest_description) 
+VALUES ('Sawtooth', 'Sawtooth forest'),
+        ('Boise', 'Boise forest'),
+        ('Payette', 'Payette forest');
