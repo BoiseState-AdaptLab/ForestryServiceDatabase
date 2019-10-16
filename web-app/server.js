98dd3a911271f077affeb010a42a09fe28fcbcab
Brenna <brennaleonard1234@gmail.com>
2019-10-15 21:49:12 -0600

    adding query builder forms to web app

--- web-app/server.js
+++ web-app/server.js
@@ -1,27 +1,72 @@
 'use strict';
 
+// ----------------------- Dependencies ----------------------- //
 const express = require('express');
+const app = express();
+const path = require('path');
+const logger = require('./modules/logger');
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'user',
+    host: 'postgres',
+    database: 'ForestServiceDB',
+    password: 'pass',
+    port: '5432'});
 
-// Constants
+// ----------------------- Constants ----------------------- //
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
+// ----------------------- App Configuration ----------------------- //
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-// App
-const app = express();
-app.get('/hello', (req, res) => {
-  res.send('Hello world\n');
+// ----------------------- Routes ----------------------- //
+
+//Homepage
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Forestry Service Database'});
 });
 
-app.get('/', function (req, res) {
+// Report summary
+app.get('/report-summary', (req, res) => {
+    res.render('report-summary', {title: 'Report Summary Query Builder'});
+});
 
-  const pg = require('pg');
-  const pool = new pg.Pool({
-    user: 'user',
-  host: 'postgres',
-  database: 'ForestServiceDB',
-  password: 'pass',
-  port: '5432'});
+app.post('/report-summary', (req, res) => {
+    logger.log.debug('report summary query posted');
+    //TODO: handle query parameters run query on database, store results in CSV
+    res.render('results', {title: 'Report Summary Data Retrieved'});
+});
+
+//Transect
+app.get('/transect', (req, res) => {
+    res.render('index', {title: 'Transect Query Builder'});
+});
+
+app.post('/transect', (req, res) => {
+    logger.log.debug('transect query posted');
+    //TODO: handle query parameters run query on database, store results in CSV
+    res.render('results', {title: 'Transect Data Retrieved'});
+});
+
+//Full report
+app.get('/full-report', (req, res) => {
+    res.render('index', {title: 'Full Report'});
+});
+
+app.post('/full-report', (req, res) => {
+    logger.log.debug('full report query posted');
+    //TODO: handle query parameters run query on database, store results in CSV
+    res.render('results', {title: 'Full Report Data Retrieved'});
+});
+
+app.get('/hello', (req,res) => {
+    res.send('Hello!');
+});
+
+//EX running query on PostGres DB
+app.get('/data', function (req, res) {
 
   pool.connect()
       .then(client => {
@@ -36,17 +81,11 @@ app.get('/', function (req, res) {
               console.log(e.stack);
             })
       }).finally(() => pool.end());
-
-
   // pool.query("SELECT NOW()", (err, res) => {
   //   console.log(err, res);
   //   pool.end();
   // });
-  
-
-
 });
 
-
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+logger.log.info(`Running on http://${HOST}:${PORT}`);
