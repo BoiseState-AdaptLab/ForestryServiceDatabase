eb07558ccc0cf2b4df83ec52fe1903175fcee54a
FloCiaglia <flociaglia@onyx.boisestate.edu>
2022-03-07 09:39:51 -0700

    Updated the function to return the most similar to the valid options

--- db/orm_scripts/pipe_get_data.py
+++ db/orm_scripts/pipe_get_data.py
@@ -7,21 +7,11 @@
 from collections import Counter
 from math import sqrt
 from config import DATABASE_URI
-from validation_functions import get_valids
-from sqlalchemy import create_engine, Column, Integer, String, inspect
-
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-import os
+from query_db import get_valid_opts
+from create_validation_tables import Session, engine, Base, ValidForest, ValidAllotment, ValidLivestock, ValidRangerDist
 
 
-engine = create_engine(DATABASE_URI)
-# inspector = inspect(engine)
-# schemas = inspector.get_schema_names()
-# for schema in schemas:
-#     print(schema)
-Session = sessionmaker(bind=engine)
-session = Session()
+local_session = Session(bind=engine)
 
 
 def word2vec(word):
@@ -44,33 +34,32 @@ def cosdis(v1, v2):
 
 
 # the guesses list comes from the pipeline side
-def get_most_similar_guess(guesses, field): 
+# the valid options come from the database
+def get_most_similar_guess(ocr_guesses, field_name): 
     '''
         This function will be called from the
         pipeline to receive the most similar
         guess of word for each field in the form.
     '''
-    
-    # correct_word = 'sheep' #TODO: retrieve this from the db <---
-    print("Right before it")
-    correct_word = get_valids(field)
-    
-    # session.query(valid_livestock).first()
+
+    field_valid_opts = get_valid_opts(field_name, local_session)
+
     best_match = [0, ]
 
-    for word in guesses:
-        # nested for loop
-        print("trial ", word)
-        word = ''.join(word).lower()
+    for word in ocr_guesses:
+        for poss in field_valid_opts:
+            # nested for loop
+            print("trial ", word)
+            word = ''.join(word).lower()
 
-        va = word2vec(word)
-        vb = word2vec(correct_word)
-        
-        prob = cosdis(va, vb)
+            va = word2vec(word)
+            vb = word2vec(poss)
+            
+            prob = cosdis(va, vb)
 
-        if prob > best_match[0]: # we probably found a match
-            best_match = [prob, word]
-        print("at this stage", best_match)
+            if prob > best_match[0]: # we probably found a match
+                best_match = [prob, word]
+            print("at this stage", best_match)
 
 
     if best_match[0] == 0.0:
@@ -84,10 +73,9 @@ def get_most_similar_guess(guesses, field):
 
 def main():
 
-    correct_word = get_valids('kind of livestock', session)
-    # best_match = get_most_similar_guess(['shep', 'cheep', 'sheep'], 'valid_livestock')
+    best_match = get_most_similar_guess(['hors', 'hoirse', 'hors'], 'kind of livestock')
     # print(best_match)
-    print(correct_word)
+    print("--- The best match is", best_match)
 
 
 if __name__ == '__main__':
