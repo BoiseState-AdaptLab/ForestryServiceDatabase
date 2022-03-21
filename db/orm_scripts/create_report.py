9c2e746844ece389ef7fa36271d622fafd26c4c4
FloCiaglia <flociaglia@onyx.boisestate.edu>
2022-03-21 15:24:10 -0600

    collects data and writes SQL insert statement to txt file

--- db/orm_scripts/create_report.py
+++ db/orm_scripts/create_report.py
@@ -10,9 +10,14 @@ sys.path.append(path.abspath('/home/FLOCIAGLIA/OCR_4_Forest_Service/ocr_4_forest
 from retrieve_Pdata import report_data, get_writeup, get_forest, get_photo
 
 def create_insert_statement():
-  what = report_data('../../../OCR_4_Forest_Service/ocr_4_forest_service/field_preds.json')
+  # change the path to the file if you want results form google vision or pipeline
+  what = report_data('OCR_4_Forest_Service/ocr_4_forest_service/google_vision_results.json')
 
-  print(what[6])
+  string = "insert into report (writeup_no, photo_no, forest, ranger_district, allotment, examiner, date, transect_no, plot_size, plot_interval, type_designation, livestock, slope, aspect, location, elevation) values (" + "'" + what[0] + "'" + ","  + "'" +what[1] + "'" + "," + "'" + what[2] + "'" + "," + "'" + what[3] + "'" + "," + "'" +what[4] + "'"+ "," + "'" +what[5]+ "'" + "," + "'" +what[6] + "'"+ "," + "'" +what[7]+ "'" + "," + "'" +what[8] + "'"+ "," + "'" +what[9]+ "'" + "," + "'" +what[10] + "'"+ "," + "'" +what[11] + "'"+ "," + "'" +what[12] + "'"+ "," + "'" +what[13] + "'"+ "," + "'" +what[14]+ "'" + "," + "'" +what[15] + "'"+ ")"
+
+  text_file = open("insert_report.txt", "w")
+  n = text_file.write(string)
+  text_file.close()
 
 
 #main function
