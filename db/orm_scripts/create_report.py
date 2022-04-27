ccc106f31758704cb6274b9f5ca748a6a9bd303c
FloCiaglia <flociaglia@onyx.boisestate.edu>
2022-04-27 11:13:37 -0600

    Fixed paths to retrieve data

--- db/orm_scripts/create_report.py
+++ db/orm_scripts/create_report.py
@@ -7,17 +7,18 @@ import os
 from os import path
 
 sys.path.append(path.abspath('/home/FLOCIAGLIA/OCR_4_Forest_Service/ocr_4_forest_service/src/'))
-from retrieve_Pdata import report_data, get_writeup, get_forest, get_photo
+from retrieve_data import report_data
 
 def create_insert_statement():
-  # change the path to the file if you want results form google vision or pipeline
-  what = report_data('OCR_4_Forest_Service/ocr_4_forest_service/google_vision_results.json')
+  # change the path to the file if you want results form google vision [google_vision_results.json] or pipeline [field_preds.json]
+  # ../OCR_4_Forest_Service/ocr_4_forest_service/
+  what = report_data('src/field_preds.json')
 
   string = "insert into report (writeup_no, photo_no, forest, ranger_district, allotment, examiner, date, transect_no, plot_size, plot_interval, type_designation, livestock, slope, aspect, location, elevation) values (" + "'" + what[0] + "'" + ","  + "'" +what[1] + "'" + "," + "'" + what[2] + "'" + "," + "'" + what[3] + "'" + "," + "'" +what[4] + "'"+ "," + "'" +what[5]+ "'" + "," + "'" +what[6] + "'"+ "," + "'" +what[7]+ "'" + "," + "'" +what[8] + "'"+ "," + "'" +what[9]+ "'" + "," + "'" +what[10] + "'"+ "," + "'" +what[11] + "'"+ "," + "'" +what[12] + "'"+ "," + "'" +what[13] + "'"+ "," + "'" +what[14]+ "'" + "," + "'" +what[15] + "'"+ ")"
 
-  text_file = open("insert_report.txt", "w")
-  n = text_file.write(string)
-  text_file.close()
+  with open(os.path.join('../Forest_Service/src/', "insert_report.txt"), "w") as text_file:
+    text_file.write(string)
+    text_file.close()
 
 
 #main function
